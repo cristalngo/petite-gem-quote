8945a27ae0d259d70fff40b67c036117b346aebe
cristalngo <cristalngo@gmail.com>
2026-06-24 10:25:11 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'petite-gem-quote-pwa-v3-20260624095748';
+const CACHE_NAME = 'petite-gem-quote-pwa-v3-20260624101223';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
